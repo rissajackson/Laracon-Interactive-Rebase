ccfdef549e9f589d57a6d4d8f593a8b2d1486f9d
Rissa <rissa@jackson.com>
2022-09-06 18:48:42 -0500

    Clean up

--- resources/views/rebase.blade.php
+++ resources/views/rebase.blade.php
@@ -5,14 +5,4 @@
 <p>commit 4</p>
 <p>commit 5</p>
 <p>commit 5</p>
-<p>Will this commit disappear?</p>
-<p>What about this one?</p>
-<p>Will this message disappear?</p>
-<p>What about this message?</p>
-<p>What happens here??</p>
-<<<<<<< HEAD
-<p>Can I add this??</p>
-=======
-<p>What about this one/?</p>
->>>>>>> 1518365... This is fixup, and this time?
 </html>
